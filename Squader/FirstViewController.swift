e19ac421056ef0cb228da32d2c98e12db5c1c236
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-02-12 01:28:26 +0300

    Initial Commit

---
+++ Squader/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Squader
+//
+//  Created by Abdulelah Hajjar on 12/02/2020.
+//  Copyright © 2020 Abdulelah Hajjar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Squader/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Squader
+//
+//  Created by Abdulelah Hajjar on 12/02/2020.
+//  Copyright © 2020 Abdulelah Hajjar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
