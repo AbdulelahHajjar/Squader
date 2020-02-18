6ce5d6c2aa554fd229c8095b1f5cb037064200b8
khaled khamis <khd.khamis@gmail.com>
2020-02-19 00:15:47 +0300

    Cells

--- Squader/MatchCell.swift
+++ Squader/MatchCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MatchCell: UITableViewCell {
     
+    //@IBOutlet weak var cellBack: UIView!
+    
+    @IBOutlet weak var cellBack: UIButton!
+    
     @IBAction func join(_ sender: Any) {
         
     }
@@ -17,6 +21,15 @@ class MatchCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        cellBack.layer.cornerRadius = 10
+        cellBack.layer.masksToBounds = false
+        
+        cellBack.layer.shadowColor = UIColor.black.cgColor
+        cellBack.layer.shadowOpacity = 0.5
+        cellBack.layer.shadowOffset = .zero
+        cellBack.layer.shadowRadius = 3
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
